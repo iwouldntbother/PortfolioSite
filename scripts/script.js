b4382867531b834a034828425df3c4cd40a16594
Will Westwood <wwstwd@gmail.com>
2021-02-16 22:09:51 +0000

    Menu Transition

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,30 @@
 var menuOpen = false;
 
 document.getElementById("menuBTN").addEventListener("click", function() {
-    console.log("menuExpand")
+    // console.log("menuExpand")
+    toggleMenu()
+})
+
+function toggleMenu() {
+
+    var menuOptions = document.getElementsByClassName("menuTrans");
+    console.log(menuOptions)
+
     if (menuOpen) {
         menuOpen = false;
-        document.getElementById("rightContainer").style.width = "5vw"
+        document.getElementById("rightContainer").style.width = "5vw";
+
+        for (let option of menuOptions) {
+            option.style.paddingLeft = "10vw";
+        }
+
     } else {
         menuOpen = true;
-        document.getElementById("rightContainer").style.width = "10vw"
+        document.getElementById("rightContainer").style.width = "10vw";
+        
+        for (let option of menuOptions) {
+            option.style.paddingLeft = "0vw";
+        }
+
     }
-})
+}
