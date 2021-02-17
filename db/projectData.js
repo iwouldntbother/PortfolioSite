51fec7cb328e687aeb6ca4d987f5de038f255c76
Will Westwood <wwstwd@gmail.com>
2021-02-17 23:24:40 +0000

    adding homepage

---
+++ db/projectData.js
@@ -0,0 +1,56 @@
+var projectData = {
+    "unit1": [
+        {
+            title: "MF Gnome",
+            subTitle: "Group Project",
+            coverImage: "/db/unit1/0/cover.jpg",
+            detail: [
+                {
+                    image: "/db/unit1/0/cover.jpg",
+                    subText: "First Sub",
+                    paraText: "First Para"
+                }
+            ]
+        }
+    ],
+
+    "unit2": [
+        {
+            title: "test1",
+            subTitle: "test1sub",
+            coverImage: "/db/unit2/0/cover.jpg",
+            detail: [
+                {
+                    image: "/db/unit1/0/cover.jpg",
+                    subText: "First Sub",
+                    paraText: "First Para"
+                },
+                {
+                    image: "/db/unit1/0/cover.jpg",
+                    subText: "First Sub",
+                    paraText: "First Para"
+                }
+            ]
+        },
+        {
+            title: "test2",
+            subTitle: "test2sub",
+            coverImage: "/db/unit2/1/cover.jpg",
+            detail: [
+                {
+                    image: "/db/unit1/0/cover.jpg",
+                    subText: "First Sub",
+                    paraText: "First Para"
+                }
+            ]
+        }
+    ],
+
+    "unit3": [
+
+    ],
+
+    "unit4": [
+
+    ]
+}
--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ document.getElementById("menuBTN").addEventListener("click", function() {
 function toggleMenu() {
 
     var menuOptions = document.getElementsByClassName("menuTrans");
-    console.log(menuOptions)
+    // console.log(menuOptions)
 
     if (menuOpen) {
         menuOpen = false;
@@ -27,4 +27,112 @@ function toggleMenu() {
         }
 
     }
+}
+
+function loadingScreen() {
+    const finalText = "Will Westwood"
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    const loadingText = document.getElementById("loadingText")
+    const loadingScreen = document.getElementById("loadingScreen")
+
+    var pos = 0;
+
+    var lsInit = setInterval(() => {
+        for (i=pos; i < finalText.length; i++) {
+            if (i == 4) {
+                loadingText.innerHTML = loadingText.innerHTML.replaceAt(i, " ")
+            } else {
+                loadingText.innerHTML = loadingText.innerHTML.replaceAt(i, alphabet[Math.floor(Math.random() * alphabet.length)])
+            }
+        }
+    }, 50)
+
+    setTimeout(() => {
+        var wipe = setInterval(() => {
+            if (pos < finalText.length) {
+                pos++;
+                loadingText.innerHTML = loadingText.innerHTML.replaceAt(pos - 1, finalText[pos - 1]);
+            } else {
+                clearInterval(lsInit);
+                clearInterval(wipe);
+            }
+        }, 250)
+    }, 2000)
+
+    setTimeout(() => {
+        loadingScreen.style.opacity = 0;
+        loadingScreen.style.pointerEvents = "none"
+    }, 7000)
+    
+}
+
+// loadingScreen()
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
+
+
+const img = document.getElementById("mouseIMG")
+const onMouseMove = (e) => {
+    img.style.left = e.pageX + 'px';
+    img.style.top = e.pageY + 'px';
+}
+
+function showCover(unitid) {
+    // console.log(id)
+    var img = document.getElementById("mouseIMG")
+
+    var unit = unitid.split("/")[0]
+    var id = unitid.split("/")[1]
+
+    img.setAttribute("src", projectData[unit][id].coverImage)
+
+    document.addEventListener("mousemove", onMouseMove);
+}
+
+function hideCover() {
+    var img = document.getElementById("mouseIMG")
+    img.setAttribute("src", "")
+    document.removeEventListener("mousemove", onMouseMove)
+}
+
+
+
+
+
+
+//
+//  Init setup
+//
+
+init()
+
+function init() {
+    let mainContentHolder = document.getElementById("mainContentHolder")
+    // document.getElementById("name").style.opacity = 0;
+
+    mainContentHolder.innerHTML = "";
+
+    mainContentHolder.innerHTML = projectList()
+
+}
+
+function projectList() {
+    var projectListData = ""
+
+    for (unit in projectData) {
+        if (projectData[unit].length < 1) {
+            break
+        }
+        // console.log(Object.values(projectData))
+        projectListData += '<div class="listSub"><h1>' + unit + '</h1></div>'
+        for (i=0; i < projectData[unit].length; i++) {
+            projectListData += '<div class="listItem" id="' + unit + "/" + i + '" onmouseover="showCover(this.id)" onmouseout="hideCover()"><h1 class="listText">' + projectData[unit][i].title + '</h1></div>'
+        }
+    }
+
+    return projectListData;
 }
