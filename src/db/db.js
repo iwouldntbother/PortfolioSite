b56be2c1acca07640b18b4ff3bf12178a1f49df1
wwstwd <wwstwd@gmail.com>
2021-08-20 15:30:43 +0100

    Addition of 0001

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,6 @@
 const db = {
   "galleryItems": [
+    // 0000
     {
       id: "0000",
       tag: "university",
@@ -38,13 +39,97 @@ const db = {
         
       ]
     },
+    // 0001
     {
       id: "0001",
       tag: "university",
       size: "span-h-2",
-      cover: "cover.jpg",
-      title: "title1",
-      date: "date"
+      cover: "/imageDB/0001/0004.jpg",
+      title: "The very best of the memories in my head",
+      date: "19.03.21 - Unit 3",
+      detail: [
+        {
+          "type": "title",
+          "data": "The very best of the memories in my head"
+        },
+        {
+          "type": "text",
+          "data": "This project was based around my personal experiences with mental health and the effect that had day to day, this is an extreme look at that, in the form of a personal diary."
+        },
+        {
+          "type": "image",
+          "data": {
+            "src": "/imageDB/0001/0001.jpg",
+            "alt": "Book cover"
+          }
+        },
+        {
+          "type": "image",
+          "data": {
+            "src": "/imageDB/0001/0002.jpg",
+            "alt": "Book cover"
+          }
+        },
+        {
+          "type": "text",
+          "data": "Each page has an image of myself manipulated digitally with hand-written text adjacent."
+        },
+        {
+          "type": "image",
+          "data": {
+            "src": "/imageDB/0001/0003.jpg",
+            "alt": "Book cover"
+          }
+        },
+        {
+          "type": "image",
+          "data": {
+            "src": "/imageDB/0001/0004.jpg",
+            "alt": "Book cover"
+          }
+        },
+        {
+          "type": "image",
+          "data": {
+            "src": "/imageDB/0001/0005.jpg",
+            "alt": "Book cover"
+          }
+        },
+        {
+          "type": "image",
+          "data": {
+            "src": "/imageDB/0001/0006.jpg",
+            "alt": "Book cover"
+          }
+        },
+        {
+          "type": "image",
+          "data": {
+            "src": "/imageDB/0001/0007.jpg",
+            "alt": "Book cover"
+          }
+        },
+        {
+          "type": "image",
+          "data": {
+            "src": "/imageDB/0001/0008.jpg",
+            "alt": "Book cover"
+          }
+        },
+        {
+          "type": "text",
+          "data": "The book has no back cover to allude to the fact that this is a continuing thing and that there may never be an end to the issues."
+        },
+        {
+          "type": "link",
+          "data": {
+                    "url": "https://google.co.uk",
+                    "target": "_blank",
+                    "text": "Link Test"
+                  }
+        },
+        
+      ]
     },
     {
       id: "0002",
