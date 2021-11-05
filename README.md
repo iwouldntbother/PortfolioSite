42c006479caa1c51231646c1aa722272986b85e9
Will Westwood <wwstwd@gmail.com>
2021-11-05 04:41:25 +0000

    Added modules to details Bibliography Captions to images

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Portfolio
 
+## TODO
 
+- [x] Add caption/source for images in detail view
+- [x] Add bibliography/citation module for detail view
--- src/components/detail.jsx
+++ src/components/detail.jsx
@@ -21,6 +21,7 @@ class Detail extends React.Component {
       </div>
     );
   }
+
 }
 
 class MainDetails extends Detail {
@@ -29,15 +30,22 @@ class MainDetails extends Detail {
       case "title":
         return <h1 className="detailTitle textColour">{ data }</h1>
       case "image":
-        return <div className="detailImage"><img src={process.env.PUBLIC_URL + data.src} alt={data.alt}></img></div>;
+        return <div className="detailImage">
+          <div>
+          <img src={process.env.PUBLIC_URL + data.src} alt={data.alt}></img>
+          <p className="detailImageCap textColour">{data.cap}</p>
+          </div>
+          </div>;
       case "sub":
         return <h2 className="detailSub textColour">{ data }</h2>
       case "text":
         return <p className="detailText textColour">{ data }</p>
       case "link":
-        return <a className="detailLink textColour" href={data.url} target={data.target}>{data.text}</a>
+        return <a className="detailLink" href={data.url} target={data.target}>{data.text}</a>
       case "spacer":
         return <div className="detailSpacer" style={{height: data+"vh"}}></div>
+      case "bibliography":
+        return <BibliographyModule bibliData={data} />
       default:
         return console.log("Error");
     }
@@ -55,6 +63,51 @@ class MainDetails extends Detail {
     }
   }
 }
+
+class BibliographyModule extends React.Component {
+
+  constructor(bibliData) {
+    super(bibliData)
+    this.bibliData = bibliData;
+  }
+
+  render() {
+    return this.bibliData.bibliData.map((item, index) => {
+      return <div>
+        <BibliographyModuleDetail bibliData={item} />
+      </div>
+    })
+  }
+}
+
+class BibliographyModuleDetail extends BibliographyModule {
+
+  constructor(bibliData) {
+    super(bibliData)
+    this.bibliData = bibliData;
+  }
+
+  render() {
+    var authorString = "";
+    for (var i=0; i< this.bibliData.bibliData.authors.length; i++) {
+      if (i === (this.bibliData.bibliData.authors.length-1)) {
+        authorString += 'and '+this.bibliData.bibliData.authors[i][1]+', '+this.bibliData.bibliData.authors[i][0].slice(0,1)+'.'
+      } else if (i === (this.bibliData.bibliData.authors.length-2)) {
+        authorString += this.bibliData.bibliData.authors[i][1]+', '+this.bibliData.bibliData.authors[i][0].slice(0,1)+'. '
+      } else {
+        authorString += this.bibliData.bibliData.authors[i][1]+', '+this.bibliData.bibliData.authors[i][0].slice(0,1)+'., '
+      }
+    }
+    
+    return <div className="detailBibliography">
+      <h3 className="bibliTitle textColour">"{this.bibliData.bibliData.title}", ({this.bibliData.bibliData.year})</h3>
+      <p className="bibliText textColour">{authorString} {this.bibliData.bibliData.publisher}.</p>
+      <span className="textColour">{this.bibliData.bibliData.site}: </span><a className="bibliURL" href={this.bibliData.bibliData.url}>{this.bibliData.bibliData.url}</a>
+      </div>
+    
+  }
+}
+
 
 function DBError() {
   return (
